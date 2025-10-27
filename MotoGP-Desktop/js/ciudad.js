ff289000fb294797ca48dbb07038fdff27c289c4
SamuelDeLaCalle <126082361+SamuelDeLaCalle@users.noreply.github.com>
2025-10-27 12:54:32 +0100

    Add files via upload

---
+++ MotoGP-Desktop/js/ciudad.js
@@ -0,0 +1,11 @@
+class Ciudad {
+
+    constructor(nombre, pais, gentilicio, poblacion, coordenadas){
+        this.nombre = nombre;
+        this.pais = pais;
+        this.gentilicio = gentilicio;
+        this.poblacion = poblacion;
+        this.coordenadas = coordenadas;
+    }
+
+}
